fc37ba5a2a7163385ac12c3de01318e06508b6f1
ermira mustafa <mustafaermira@gmail.com>
2022-04-10 01:09:33 +0200

    setup

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+/*
+Krejt aplikacioni ka me qene brenda App component dhe kete komponente
+e bejme hook brenda root
+Root eshte brenda folderit public, ne file-in index.html
+*/
