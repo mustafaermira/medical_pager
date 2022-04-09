c13f409eec3b11f0bf84348877ca787600e833eb
ermira mustafa <mustafaermira@gmail.com>
2022-04-10 01:41:35 +0200

    first start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {StreamChat} from 'stream-chat';
+import{Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
+import { ChannelListContainer, ChannelContainer } from './components';
+
+const apiKey='g7jcw9d7nms2'; //me initialize chat
+
+const client = StreamChat.getInstance(apiKey);
 const App = () => {
   return (
-    <div>
-        <h1>Medical Pager chat app</h1>
+    <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+          <ChannelListContainer/>
+          <ChannelContainer/>
+        </Chat>
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 
 import App from './App';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+//ReactDOM.render(<App/>,document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render (
+    <StrictMode>
+        <App/>
+    </StrictMode>
+)
 /*
 Krejt aplikacioni ka me qene brenda App component dhe kete komponente
 e bejme hook brenda root
