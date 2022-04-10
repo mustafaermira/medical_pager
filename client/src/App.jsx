4a817a6f42e05ce07b46d2a762054e182517e13c
ermira mustafa <mustafaermira@gmail.com>
2022-04-10 23:38:28 +0200

    LogIn&SignUp(front)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,18 @@ import {StreamChat} from 'stream-chat';
 import{Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css';
 
 const apiKey='g7jcw9d7nms2'; //me initialize chat
 
 const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
+
 const App = () => {
+
+  if(!authToken) return <Auth/>
   return (
     <div className="app__wrapper">
         <Chat client={client} theme="team light">
